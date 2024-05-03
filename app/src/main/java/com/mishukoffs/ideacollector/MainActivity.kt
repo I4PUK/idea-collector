c4deefb639eaca0e92da740b0b5a9f770849acb8
mishukoffs <Mis.Serj@yandex.ru>
2024-05-03 23:16:46 +0300

    Добавлен выпадающий спинер

--- app/src/main/java/com/mishukoffs/ideacollector/MainActivity.kt
+++ app/src/main/java/com/mishukoffs/ideacollector/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.mishukoffs.ideacollector
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ImageButton
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.GridLayoutManager
@@ -10,6 +12,7 @@ import com.mishukoffs.ideacollector.model.IdeaModel
 import com.mishukoffs.ideacollector.model.IdeaStatus
 import java.util.Calendar
 import java.util.Date
+
 
 class MainActivity : ComponentActivity() {
 
@@ -43,16 +46,24 @@ class MainActivity : ComponentActivity() {
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
         val ideaTextView: TextView = findViewById(R.id.idea_text_field)
         val addIdeaButton: ImageButton = findViewById(R.id.add_idea_button)
+        val statusesList: Spinner = findViewById(R.id.status_dropdown)
 
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = GridLayoutManager(this, 1)
         recyclerView.adapter = customAdapter
+
+        statusesList.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            IdeaStatus.entries
+        )
 
         addIdeaButton.setOnClickListener {
             val ideaTextField: String = ideaTextView.text.toString()
             if (ideaTextField.trim().isNotEmpty()) {
                 list.add(
                     IdeaModel(
+                        // statusesList.getSelectedItem()
                         title = ideaTextField,
                         status = IdeaStatus.MEDIUM,
                         createdDate = getNowDate()
@@ -65,6 +76,8 @@ class MainActivity : ComponentActivity() {
                 customAdapter.notifyDataSetChanged()
             }
         }
+
+
     }
 
     fun getNowDate(): Date {
